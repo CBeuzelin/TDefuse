358a9872b3cb3f43679b416a61b017ff351b7562
CBeuzelin <beuzelincharly@gmail.com>
2019-08-26 20:43:49 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-npm run build
-mkdir -p /var/www/html/TDefuse
-cp -r ./dist/TDefuse/* /var/www/html/TDefuse/
+﻿npm run build
+mkdir -p /var/www/html/TDefuse/
+cp -r ./dist/TDefuse/* /var/www/html/TDefuse
 systemctl restart nginx.service
